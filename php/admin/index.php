3767818c1280a077e9f1769a8428eb8fc8c556c7
KoolNH <lankool2406@gmail.com>
2023-10-17 15:07:41 +0700

    index

--- php/admin/index.php
+++ php/admin/index.php
@@ -42,25 +42,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
- <!-- Start Dashboard Top  -->
- <div class="rbt-dashboard-content-wrapper">
-                        <div class="tutor-bg-photo bg_image bg_image--22 height-350"></div>
-                        <!-- Start Tutor Information  -->
-                        <div class="rbt-tutor-information">
-                            <div class="rbt-tutor-information-left">
-                                <div class="thumbnail rbt-avatars size-lg">
-                                    <img src="../assets/images/team/avatar.jpg" alt="Instructor">
-                                </div>
-                                <div class="tutor-content">
-                                    <h5 class="title">John Due</h5>
-                                   
-                                </div>
-                            </div>
 
-                        </div>
-                        <!-- End Tutor Information  -->
-                    </div>
-                    <!-- End Dashboard Top  -->
 
                     <div class="row g-5">
                         <div class="col-lg-3">
