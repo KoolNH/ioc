fe87300ed2a958ec4a073c8029db09370c38f294
Dennis Nguyen <congnv8192@gmail.com>
2023-10-17 15:09:07 +0700

    '.'

--- php/admin/inc/footer.php
+++ php/admin/inc/footer.php
@@ -1,81 +1,12 @@
 <footer class="rbt-footer footer-style-1 bg-color-white overflow-hidden">
-        <div class="footer-top">
-            <div class="container">
-                <div class="row g-5">
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="footer-widget">
-                            <div class="logo">
-                                <a href="index.html">
-                                    <img src="../assets/images/logo/logo.png" alt="Edu-cause">
-                                </a>
-                            </div>
-
-                            <p class="description mt--20">We’re always in search for talented
-                                and motivated people. Don’t be shy introduce yourself!
-                            </p>
-
-                            <ul class="social-icon social-default justify-content-start">
-                                <li><a href="https://www.facebook.com/">
-                                        <i class="feather-facebook"></i>
-                                    </a>
-                                </li>
-                                <li><a href="https://www.twitter.com">
-                                        <i class="feather-twitter"></i>
-                                    </a>
-                                </li>
-                                <li><a href="https://www.instagram.com/">
-                                        <i class="feather-instagram"></i>
-                                    </a>
-                                </li>
-                                <li><a href="https://www.linkdin.com/">
-                                        <i class="feather-linkedin"></i>
-                                    </a>
-                                </li>
-                            </ul>
-
-                            <div class="contact-btn mt--30">
-                                <a class="rbt-btn hover-icon-reverse btn-border-gradient radius-round" href="#">
-                                    <div class="icon-reverse-wrapper">
-                                        <span class="btn-text">Contact With Us</span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                        <span class="btn-icon"><i class="feather-arrow-right"></i></span>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-   
-
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="footer-widget">
-                            <h5 class="ft-title">Get Contact</h5>
-                            <ul class="ft-link">
-                                <li><span>Phone:</span> <a href="#">(406) 555-0120</a></li>
-                                <li><span>E-mail:</span> <a href="mailto:hr@example.com">admin@example.com</a></li>
-                            </ul>
-
-
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="footer-widget">
-                            <img src ="../assets/images/course/1.jpg">
-                        
-
-
-                        </div>
-                    </div>
+    <div class="container p-5">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="footer-widget">
+                    <p class="description">All rights reserved IoC 2023
+                    </p>
                 </div>
             </div>
         </div>
-        <div class="rbt-separator-mid">
-            <div class="container">
-                <hr class="rbt-separator m-0">
-            </div>
-        </div>
-
-    </footer>
+    </div>
+</footer>
--- php/admin/inc/side-bar.php
+++ php/admin/inc/side-bar.php
@@ -1,27 +1,22 @@
 <div class="rbt-default-sidebar sticky-top rbt-shadow-box rbt-gradient-border">
-                                <div class="inner">
-                                    <div class="content-item-content">
+    <div class="inner">
+        <div class="content-item-content">
 
-                                        <div class="rbt-default-sidebar-wrapper">
-                                            <div class="section-title mb--20">
-                                                <h6 class="rbt-title-style-2">Welcome, Admin</h6>
-                                            </div>
-                                            <nav class="mainmenu-nav">
-                                                <ul class="dashboard-mainmenu rbt-default-sidebar-list">
-                                                    <li><a href="/admin"><i class="feather-home"></i><span>Dashboard</span></a></li>
-                                                    <li><a href="/admin/profile.php"><i class="feather-user"></i><span>My Profile</span></a></li>
-                                                    <li><a href="/admin/courses.php"><i class="feather-book-open"></i><span>Courses</span></a></li>
-                                                    <li><a href="/admin/learners.php"><i class="feather-book-open"></i><span>Learner</span></a></li>
-                                                    <li><a href="/admin/instructors.php"><i class="feather-book-open"></i><span>Instructor</span></a></li>
-
-                                                </ul>
-                                            </nav>
-
-
-                                                
-                                         
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
+            <div class="rbt-default-sidebar-wrapper">
+                <div class="section-title mb--20">
+                    <h6 class="rbt-title-style-2">Welcome, Admin</h6>
+                </div>
+                <nav class="mainmenu-nav">
+                    <ul class="dashboard-mainmenu rbt-default-sidebar-list">
+                        <li><a href="/admin"><i class="feather-home"></i><span>Dashboard</span></a></li>
+                        <li><a href="/admin/profile.php"><i class="feather-user"></i><span>My Profile</span></a></li>
+                        <li><a href="/admin/courses.php"><i class="feather-book-open"></i><span>Courses</span></a></li>
+                        <li><a href="/admin/learners.php"><i class="feather-book-open"></i><span>Learner</span></a></li>
+                        <li><a href="/admin/instructors.php"><i
+                                    class="feather-book-open"></i><span>Instructor</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
